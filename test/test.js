36063fc6cc19031585a0a871a3245e523b93d0db
Russ Bradberry <rbradberry@gmail.com>
2012-01-02 18:47:38 -0500

    initial cql support

--- test/test.js
+++ test/test.js
@@ -10,11 +10,18 @@ pool.connect('helenus_test', function(err, keyspace){
   if(err){
     throw(err);
   } else {    
+    //test standard
     keyspace.test.insert('foo:bar', { 'hello:world':'cccc' }, {}, function(err, something){
       console.log(err, something);
     });
     
+    //test composite
     keyspace.composite_test.insert('1325541869796:bar', { '1325541869796:world':132 }, {}, function(err, something){
+      console.log(err, something);
+    });
+    
+    //test cql
+    pool.cql('UPDATE test SET cqlfoo=\'%s\' WHERE KEY=\'%s\'', 'cqlbar','cqlkey', function(err, something){
       console.log(err, something);
     });
   }
